791c798e09c40108556ef97ffb3ae3f29c03c09d
Wellington Junior <Wellington Junior>
2023-03-09 12:02:07 -0300

    API Composition Pattern em Authuser Microservice – metade da 2

--- src/main/java/com/ead/authuser/clients/UserClient.java
+++ src/main/java/com/ead/authuser/clients/UserClient.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
 
 @Component
 public class UserClient {
@@ -26,7 +30,11 @@ public class UserClient {
                 pageable.getSort().toString().replaceAll(": ", ",");
 
         try {
-
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {
+            };
+            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null,
+                    responseType);
+            searchResult = result.getBody().getContent();
         } catch (Exception e) {
             // TODO: handle exception
         }
